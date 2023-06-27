4e6c2bddc2bbdca027b4c67333b7ba6ebaa0af97
Djoudi21 <abdelkrim@gdpr.dev>
2023-06-27 22:49:51 +0200

    feat: add routes and screens folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Text } from "react-native";
-import {store, persistor} from './store';
-import {PersistGate} from 'redux-persist/integration/react';
+import { Text } from 'react-native';
+import { persistor, store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import Home from './components/Home'
+import HomeScreen from './screens/HomeScreen';
+
 function App(): React.ReactElement {
   return (
     <Provider store={store}>
       <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
-        <Home />
+        <HomeScreen />
       </PersistGate>
     </Provider>
   );
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import { SafeAreaView, Text } from 'react-native';
 import React from 'react';
 
-export default function Home() {
+export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Text>Home</Text>
