5754138235089bb4f5f61bca1ba9fc1aa9b68fa5
Djoudi21 <abdelkrim@gdpr.dev>
2023-06-28 00:28:43 +0200

    feat: add auth hook and screens

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,134 @@
+import { useRef, useState } from 'react';
+// import { useDispatch } from 'react-redux';
+
+export default function useAuth({ navigation }: any) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [emailErrorMessage, setEmailErrorMessage] = useState('');
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+  const [formSubmissionErrorMessage, setFormSubmissionErrorMessage] =
+    useState('');
+  // const dispatch = useDispatch();
+  // const axiosUserRepository = new AxiosUserRepository();
+  // const userService = new UserService(axiosUserRepository);
+  const emailTextInputRef = useRef(null);
+  const passwordTextInputRef = useRef(null);
+
+  async function handleSubmit() {
+    if (emailTextInputRef.current) {
+      // @ts-ignore
+      emailTextInputRef.current.blur();
+    }
+
+    if (passwordTextInputRef.current) {
+      // @ts-ignore
+      passwordTextInputRef.current.blur();
+    }
+
+    emailValidation();
+    passwordValidation();
+    if (!isEmailValid || !isPasswordValid) {
+      return;
+    }
+    const credentials = {
+      email,
+      password,
+    };
+    try {
+      // const response = await userService.login(credentials);
+      // const tokens = response.data;
+      // dispatch(setTokens(tokens));
+      // navigation.push('Home');
+      //fetch user thcats and dispatch in store
+      //set current tchat id to be an tchat with no messages
+    } catch (e) {
+      // @ts-ignore
+      const errorMessage = setSubmissionErrorMessage(e.response.data.message);
+      setFormSubmissionErrorMessage(errorMessage);
+    }
+  }
+
+  function setInputBorderColor(inputName: string) {
+    const valid = {
+      borderColor: '#212121',
+    };
+
+    const invalid = {
+      borderColor: 'red',
+    };
+    if (inputName === 'email') {
+      return isEmailValid ? valid : invalid;
+    } else {
+      return isPasswordValid ? valid : invalid;
+    }
+  }
+
+  function emailValidation() {
+    if (!email.length) {
+      setIsEmailValid(false);
+      setEmailErrorMessage('Please enter an email');
+      return;
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+      setIsEmailValid(false);
+      setEmailErrorMessage('Email is not valid');
+      return;
+    } else {
+      setIsEmailValid(true);
+    }
+  }
+
+  function passwordValidation() {
+    if (!password.length) {
+      setIsPasswordValid(false);
+      setPasswordErrorMessage('Please enter a paswword');
+      return;
+    } else {
+      setIsPasswordValid(true);
+    }
+  }
+
+  function setSubmissionErrorMessage(message: string): string {
+    switch (message) {
+      default:
+        return "Aucun utilisateur n'a été trouvé";
+    }
+  }
+
+  function handleRedirect(routeName: string) {
+    navigation.push(routeName);
+  }
+
+  function handleFocusEmailInput() {
+    setEmail('');
+    setFormSubmissionErrorMessage('');
+  }
+
+  function handleFocusPasswordInput() {
+    setPassword('');
+    setFormSubmissionErrorMessage('');
+  }
+
+  return {
+    handleSubmit,
+    setInputBorderColor,
+    emailValidation,
+    passwordValidation,
+    setSubmissionErrorMessage,
+    handleRedirect,
+    handleFocusPasswordInput,
+    handleFocusEmailInput,
+    emailErrorMessage,
+    passwordErrorMessage,
+    emailTextInputRef,
+    passwordTextInputRef,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    isEmailValid,
+    isPasswordValid,
+    formSubmissionErrorMessage,
+  };
+}
--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,10 +1,113 @@
-import { Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 import React from 'react';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+import useAuth from '../hooks/useAuth';
 
-export function RegisterScreen() {
+export function RegisterScreen({ navigation }: any) {
+  const auth = useAuth({ navigation });
+
   return (
-    <View>
-      <Text>REGISTER</Text>
+    <View style={styles.container}>
+      <Text>Inscrivez-vous</Text>
+      <View style={[styles.inputContainer]}>
+        <TextInput
+          ref={auth.emailTextInputRef}
+          onBlur={() => auth.emailValidation()}
+          placeholder="Email"
+          onFocus={auth.handleFocusEmailInput}
+          style={[styles.input, auth.setInputBorderColor('email')]}
+          onChangeText={auth.setEmail}
+          value={auth.email}
+        />
+        {!auth.isEmailValid && (
+          <View style={styles.errorContainer}>
+            {/*<Icon name="exclamation-circle" size={20} color={'red'} />*/}
+            <Text style={styles.errorMessage}>{auth.emailErrorMessage}</Text>
+          </View>
+        )}
+      </View>
+      <View style={[styles.inputContainer]}>
+        <TextInput
+          ref={auth.passwordTextInputRef}
+          onBlur={() => auth.passwordValidation()}
+          placeholder="Password"
+          onFocus={auth.handleFocusPasswordInput}
+          style={[styles.input, auth.setInputBorderColor('password')]}
+          onChangeText={auth.setPassword}
+          value={auth.password}
+          secureTextEntry={true}
+        />
+        {!auth.isPasswordValid && (
+          <View style={styles.errorContainer}>
+            {/*<Icon name="exclamation-circle" size={20} color={'red'} />*/}
+            <Text style={styles.errorMessage}>{auth.passwordErrorMessage}</Text>
+          </View>
+        )}
+      </View>
+
+      <Pressable onPress={auth.handleSubmit} style={styles.button}>
+        <Text style={styles.labelButton}>Validez</Text>
+      </Pressable>
+
+      {auth.formSubmissionErrorMessage &&
+        auth.isEmailValid &&
+        auth.isPasswordValid && (
+          <Text style={styles.errorMessage}>
+            {auth.formSubmissionErrorMessage}
+          </Text>
+        )}
+
+      <View>
+        <Text>Déja un compte ?</Text>
+        <Pressable onPress={() => auth.handleRedirect('Login')}>
+          <Text style={styles.registerLink}>Connectez-vous</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FDFDFD',
+    gap: 20,
+  },
+  inputContainer: {
+    width: '50%',
+  },
+  input: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+  },
+  button: {
+    backgroundColor: '#212121',
+    borderRadius: 5,
+    width: '50%',
+    padding: 10,
+  },
+  labelButton: {
+    color: '#FDFDFD',
+    textAlign: 'center',
+  },
+  errorContainer: {
+    marginTop: 5,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  errorMessage: {
+    color: 'red',
+  },
+  registerLink: {
+    color: '#212121',
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+});
